5f9b517c762c564dda798a4b1848b1602e3a4b90
Nicolas LARESSER <nicolas.laresser@gmail.com>
2021-08-24 12:28:25 +0200

    Div Fix
    MAJ bootsrap
    Fix Form

---
+++ application/config/app.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') || exit('No direct script access allowed');
+
+/* APP STUFF */
+$config['app_name'] = 'Membres BN3F';
+$config['slogan'] 	= 'Outil de gestion des membres de la BN3F';
+$config['about'] 	= 'By NL';
+$config['debug_app']= 'none'; //none,debug,profiler
+$config['sidebar'] = 'on';
+
+$config['crlf'] = '';
+
+/* EMAIL */
+$config['protocol'] = "smtp";
+$config['smtp_host'] = "smtp.1and1.com";
+$config['smtp_port'] = "587";
+$config['smtp_user'] = "noreply@domain.com";
+$config['smtp_pass'] = "noreply";
+$config['smtp_crypto'] = 'tls';
+$config['charset'] = 'utf-8';
+$config['mailtype'] = 'html';
+$config['wordwrap'] = TRUE;
+$config['newline'] = "\r\n";
+$config['crlf'] = "\r\n";
+
+
